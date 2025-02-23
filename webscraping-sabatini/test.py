06aa7cbf73678fff6ec8a46e4d8dd60ff3bb9637
Francisco-byte-crypto <franciscojoin@gmail.com>
2025-02-23 10:23:32 -0300

    Delete webscraping-sabatini/test.py

--- webscraping-sabatini/test.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.support import expected_conditions as EC
-from driver.create_web_driver import create_web_driver
-from scraper.Page_Context import Page_Context
-from scraper.Page_Content import Page_Content
-from processing.clean_dataframe_data import clean_dataframe_data 
-from selenium.webdriver.common.by import By
-
-def _test_():
-_test_()
